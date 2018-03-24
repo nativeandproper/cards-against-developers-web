9e6737e7118b52cb00cc916b7df5d86ef88d362f
Carl Bernardo <carlbernrdo@gmail.com>
2018-03-24 12:23:43 -0700

    style: add link back to home page in SignupConfirmation

--- src/containers/SignupConfirmation.tsx
+++ src/containers/SignupConfirmation.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { Link, RouteComponentProps } from 'react-router-dom';
 
 // Styles
 import '../styles/SignupConfirmation.css';
@@ -13,15 +13,18 @@ export default class SignupConfirmation extends React.Component<SignupConfirmati
         <h1>
           Hey Bud!
         </h1>
+        <div className="thanks-bud-img" />
         <h1>
           {`(sorry ${this.props.location.state.firstName}, we're not actually buds just yet)`}
         </h1>
-        <div className="thanks-bud-img" />
         <div className="confirmation-copy">
           A verification link has been sent to your email account.
         </div>
         <div className="confirmation-copy">
           You have 24 hours to click on the verification link to complete account registration. If you do not receive an email within 24 hours, please  <a href="mailto:nativeandproper@gmail.com">contact us</a>.
+        </div>
+        <div className="confirmation-copy">
+          Click <Link to="/" className="cta-link">here</Link> to go back to the home page.
         </div>
       </div>
     );
