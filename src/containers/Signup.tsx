bf2a3399224f532105be026c43f1f5b31f71cb8c
Carl Bernardo <carlbernrdo@gmail.com>
2018-03-23 18:36:04 -0700

    feat: add validation to Signup component, create string validation helpers

--- src/containers/Signup.tsx
+++ src/containers/Signup.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
+import {
+  invalidFirstName,
+  invalidLastName,
+  invalidEmail,
+  invalidPassword
+} from '../helpers/stringHelper';
+
 // Styles
 import '../styles/LoginSignup.css';
 
@@ -10,6 +17,11 @@ export interface SignupState {
   lastName: string;
   email: string;
   password: string;
+  attemptedSubmit: boolean;
+  firstNameHasError: boolean;
+  lastNameHasError: boolean;
+  emailHasError: boolean;
+  passwordHasError: boolean;
 }
 
 export default class Signup extends React.Component<SignupProps, Partial<SignupState>> {
@@ -20,7 +32,12 @@ export default class Signup extends React.Component<SignupProps, Partial<SignupS
       firstName: '',
       lastName: '',
       email: '',
-      password: ''
+      password: '',
+      attemptedSubmit: false,
+      firstNameHasError: false,
+      lastNameHasError: false,
+      emailHasError: false,
+      passwordHasError: false
     };
   }
 
@@ -30,37 +47,78 @@ export default class Signup extends React.Component<SignupProps, Partial<SignupS
     });
   }
 
-  validateAndSubmit = () => {
-    console.log('signup data to POST: ', this.state);
+  isValidForm = () => {
+    const firstNameHasError = invalidFirstName(this.state.firstName);
+    const lastNameHasError = invalidLastName(this.state.lastName);
+    const emailHasError = invalidEmail(this.state.email);
+    const passwordHasError = invalidPassword(this.state.password);
+    
+    this.setState({
+      firstNameHasError,
+      lastNameHasError,
+      emailHasError,
+      passwordHasError
+    });
+
+    return !firstNameHasError && !lastNameHasError && !emailHasError && !passwordHasError;
+  }
+
+  validateAndSubmit = (e: React.SyntheticEvent<HTMLElement>) => {
+    this.setState({
+      attemptedSubmit: true
+    });
+
+    if (this.isValidForm()) {
+      console.log('signup data to POST: ', this.state);
+    } else {
+      e.preventDefault();
+    }
   }
 
   render() {
+    const firstNameErrorClasses = this.state.attemptedSubmit && this.state.firstNameHasError ? `error error-show` : `error`;
+    const lastNameErrorClasses = this.state.attemptedSubmit && this.state.lastNameHasError ? `error error-show` : `error`;
+    const emailErrorClasses = this.state.attemptedSubmit && this.state.emailHasError ? `error error-show` : `error`;
+    const passwordErrorClasses = this.state.attemptedSubmit && this.state.passwordHasError ? `error error-show` : `error`;
+
     return (
       <div className="login-signup">
         <h1>
           Sign Up
         </h1>
         <div className="login-signup-form">
+          <div className={firstNameErrorClasses}>
+            Your first name cannot be empty and must be less than 32 characters
+          </div>
           <input
             type="text"
             placeholder="first name"
             value={this.state.firstName}
             onChange={(e) => this.handleChange('firstName', e.target.value)}
           />
+          <div className={lastNameErrorClasses}>
+            Your last name cannot be empty and must be less than 32 characters
+          </div>
           <input
             type="text"
             placeholder="last name"
             value={this.state.lastName}
             onChange={(e) => this.handleChange('lastName', e.target.value)}
           />
+          <div className={emailErrorClasses}>
+            Your email must be a valid email e.g. swaglord@gmail.com
+          </div>
           <input
             type="text"
             placeholder="email"
             value={this.state.email}
             onChange={(e) => this.handleChange('email', e.target.value)}
           />
+          <div className={passwordErrorClasses}>
+            Your password must be between 5 and 32 characters
+          </div>
           <input
-            type="text"
+            type="password"
             placeholder="password"
             value={this.state.password}
             onChange={(e) => this.handleChange('password', e.target.value)}
---
+++ src/helpers/stringHelper.ts
@@ -0,0 +1,22 @@
+import * as R from 'ramda';
+
+export const invalidFirstName = (firstName: string): boolean => {
+  return R.isEmpty(firstName) || R.gt(firstName.length, 32);
+};
+
+export const invalidLastName = (lastName: string): boolean => {
+  return R.isEmpty(lastName) || R.gt(lastName.length, 32);
+};
+
+const isValidEmail = (email: string): boolean => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+};
+
+export const invalidEmail = (email: string): boolean => {
+  return R.isEmpty(email) || R.gt(email.length, 32) || !isValidEmail(email);
+};
+
+export const invalidPassword = (password: string): boolean => {
+  return R.isEmpty(password) || R.lt(password.length, 5) || R.gt(password.length, 32);
+};
